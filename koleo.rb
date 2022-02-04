f2921c8800c6cb11395c01d7da83be8d8499ceed
temidora <97547666+temidora@users.noreply.github.com>
2022-02-04 20:58:12 +0100

    Update koleo.rb

--- koleo.rb
+++ koleo.rb
@@ -1,34 +1,66 @@
-
 puts "Enter value and press enter or type EXIT to finish "
-
 #creates an array
 command=""
+#creates hash with names and values 
 dictionary=Hash.new()
+#creates array of hashes 
+list=Array.new()
+list.push(dictionary)
+#iterator of hash in array
+i=0 
 
 while (command!= "EXIT") 
     command= gets.chomp
     separated_words=command.split()
     
   if separated_words[0]=="SET"
-     dictionary[separated_words[1]]=separated_words[2]
+     list[i][separated_words[1]]=separated_words[2]
     
   elsif separated_words[0]=="GET"
-     if dictionary.has_key?(separated_words[1])
-       puts dictionary[separated_words[1]]
+     if list[i].has_key?(separated_words[1])
+       puts list[i][separated_words[1]]
      else 
        puts "NULL"
      end
+  
   elsif separated_words[0]=="DELETE"
-      dictionary.delete(separated_words[1])
+      list[i].delete(separated_words[1])
+    
   elsif separated_words[0]=="COUNT"
-     if dictionary.value?(separated_words[1])
+     if list[i].value?(separated_words[1])
        #puts dictionary.key(separated_words[1])
-       puts dictionary.values.count(separated_words[1])
+       puts list[i].values.count(separated_words[1])
      else 
        puts 0
      end
- 
-  end  
+  
+  elsif separated_words[0]=="BEGIN"
+    dict_temp = list[i].clone   
+    list.push(dict_temp)
+      i+=1
+
+  elsif separated_words[0]=="PRT"
+      puts list
+      puts i 
+
+  elsif separated_words[0]=="ROLLBACK"
+      if i==0
+        puts "NO TRANSACTION"
+      else
+        list.pop
+        i-=1
+      end 
+  
+  elsif separated_words[0]=="COMMIT"
+    if i==0
+      puts "NO TRANSACTION"
+    else
+      list[i-1]=list[i]
+      list.pop
+      i-=1
+    end
+  end
 end   
 
-puts dictionary
+#puts dictionary
+puts list
