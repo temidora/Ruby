37e415d69de98a3ae04b1073d4c0d31d78223c3f
temidora <97547666+temidora@users.noreply.github.com>
2022-02-04 12:36:50 +0100

    Update koleo.rb

--- koleo.rb
+++ koleo.rb
@@ -9,16 +9,25 @@ while (command!= "EXIT")
     command= gets.chomp
     separated_words=command.split()
     
-    if separated_words[0]=="SET"
-        dictionary[separated_words[1]]=separated_words[2]
+  if separated_words[0]=="SET"
+     dictionary[separated_words[1]]=separated_words[2]
     
-    elsif separated_words[0]=="GET"
-      if dictionary.has_key?(separated_words[1])
-        puts dictionary[separated_words[1]]
-      else 
-        puts "NULL"
-      end
-    end 
+  elsif separated_words[0]=="GET"
+     if dictionary.has_key?(separated_words[1])
+       puts dictionary[separated_words[1]]
+     else 
+       puts "NULL"
+     end
+  elsif separated_words[0]=="DELETE"
+      dictionary.delete(separated_words[1])
+  elsif separated_words[0]=="COUNT"
+     if dictionary.has_value?(separated_words[1])
+       puts dictionary.key(separated_words[1])
+     else 
+       puts 0
+     end
+  
+  end  
 end   
 
 puts dictionary
