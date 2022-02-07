eeda62e99fce059e83c3665a08a32aead2365b9d
temidora <97547666+temidora@users.noreply.github.com>
2022-02-07 18:39:58 +0100

    Update StringChallenge2.rb

--- StringChallenge2.rb
+++ StringChallenge2.rb
@@ -1,9 +1,25 @@
 def StringChallenge(str)
-
-  # code goes here
-  return str
-
+i=0
+  while i<=str.length-1
+     if str[i]!=str[i+1]
+       
+        case str[i..i+1]
+        when "ab" || "ba"
+            str[i..i+1]="c"
+            puts "changed to c: #{str}"
+        when "ac" || "ca"
+            str[i..i+1]="b"
+            puts "changed to: 2 #{str}"
+        when "bc" || "cb"
+            str[i..i+1]="a"
+            puts "changed to: a #{str}"
+        end
+        i=0
+     else 
+        i+=1
+     end
+  end
+puts str.length
 end
 
-# keep this function call here 
-puts StringChallenge(STDIN.gets)
+StringChallenge("abcabc")
